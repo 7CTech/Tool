da54996ce92dd45ccbd935b107433a3a8078755e
7CTech <ctech.exe@gmail.com>
2017-01-01 06:47:08 +0200

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -117,7 +117,7 @@ function debug() {
   find ./ -maxdepth 1 -perm /a+x -type f -exec cp {} $rootDir/debug/ \;
   cd $rootDir
   echo "Copying Sources"
-  cp *.c *.h $rootDir/debug/sources
+  cp *.c *.h *.cpp *.cxx *.hpp ./debug/sources
 
 
 ##################################################
